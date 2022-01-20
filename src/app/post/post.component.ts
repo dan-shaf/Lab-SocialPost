77ab77326dd94226e4f81192f7524a9e581e7978
captwryn <93493592+captwryn@users.noreply.github.com>
2022-01-20 09:38:05 -0500

    initial

---
+++ src/app/post/post.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Post } from '../post';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit {
+@Input() thepost: Post = {
+  title: 'test title',
+  thought: 'test thought'
+}
+  @Output() delete = new EventEmitter<Post>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  deleteMe() {
+    this.delete.emit(this.thepost);
+  }
+
+}
---
+++ src/app/social-posts/social-posts.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Post } from '../post';
+
+
+@Component({
+  selector: 'app-social-posts',
+  templateUrl: './social-posts.component.html',
+  styleUrls: ['./social-posts.component.css']
+})
+export class SocialPostsComponent implements OnInit {
+
+  
+  posts: Post[] = [
+    {title: 'first', thought: 'some thoughts'},
+    {title: 'second', thought: 'more thoughts'}
+  ];
+
+  newtitle: string = "";
+  newthought: string = "";
+  createPostVisible: boolean = false;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  savePost() {
+    this.posts.unshift(
+      { title: this.newtitle, thought: this.newthought}
+    )
+    this.createPostVisible = false;
+  }
+  showCreateForm() {
+    this.newtitle = '';
+    this.newthought = '';
+    this.createPostVisible= true;
+    
+  }
+  deletePost(thepost: Post) {
+    for (let index = 0; index < this.posts.length; index++) {
+      if(this.posts[index] == thepost) {
+          this.posts.splice(index, 1);
+      }
+    }
+  }
+}
